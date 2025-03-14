bb283270449f76a5822083280bc071428e5119fc
Cristian3016 <cristiancaro163@gmail.com>
2025-03-14 20:21:40 -0300

    Movimiento del proyectile

--- characters/player/hand_anchor.gd
+++ characters/player/hand_anchor.gd
@@ -6,4 +6,4 @@ extends Node2D
 func _process(delta: float) -> void:
 	var cursor_position = get_global_mouse_position()
 	var rotation_angle = global_position.angle_to_point(cursor_position)
-	rotation = rotation_angle
+	rotation = -rotation_angle
